8930546849418c5118cbcae08f9d4710115ae762
aadvisingh04 <aadvisingh01@gmail.com>
2025-04-20 11:19:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,25 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
-
         HashMap<Character,Integer> map=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-           char ch = s.charAt(i);
+        for(char ch:s.toCharArray()){
            map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        for(int i=0;i<t.length();i++){
-           char ch = t.charAt(i);
-           if(!map.containsKey(ch)) return false;
-           int count=map.get(ch);
-
-            if(count==1){
-                map.remove(ch);
-            }
-            else{
-                map.put(ch,count-1);
-            }                    
-            
+        for(char ch:t.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1); 
         }
-        return map.isEmpty();
+        for(int val:map.values()){
+            if(val!=0) return false;
+        }
+        return true;
     }
 }
