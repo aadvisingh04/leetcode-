5e06a67fd001e2c24abacade568e97e1c64923de
aadvisingh04 <aadvisingh01@gmail.com>
2025-04-20 11:36:16 +0530

    Time: 4 ms (75.95%), Space: 45.1 MB (7.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public boolean isAnagram(String s, String t) {
         HashMap<Character,Integer> map=new HashMap<>();
         for(char ch:s.toCharArray()){
@@ -10,6 +10,27 @@ class Solution {
         for(int val:map.values()){
             if(val!=0) return false;
         }
+        return true;
+    }
+}*/
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // Case 1: Lengths must be equal
+        if (s.length() != t.length()) return false;
+
+        // Convert strings to char arrays and sort them
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+
+        // Compare sorted arrays
+        for (int i = 0; i < sArr.length; i++) {
+            if (sArr[i] != tArr[i]) return false;
+        }
+
         return true;
     }
 }
