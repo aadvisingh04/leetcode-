e9e6ff46efb3b6816aded0553c8d700a48e58efa
Aadvijyotirma Singh Deo <aadvisingh01@gmail.com>
2026-01-21 02:58:32 +0530

    Time: 1 ms (100%), Space: 47 MB (11.4%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.java
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.java
@@ -1,34 +1,13 @@
-import java.util.*;
-
 class Solution {
-    private boolean isPrime(int n) {
-        if (n <= 1) return false;
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) return false;
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int[] res = new int[nums.size()];
+        for (int i = 0; i < nums.size(); i++) {
+            int n = nums.get(i);
+            if ((n & 1) == 1)
+                res[i] = n & ~(((n + 1) & ~n) >> 1);
+            else
+                res[i] = -1;
         }
-        return true;
-    }
-  public int[] minBitwiseArray(List<Integer> nums) {
-        int[] ans = new int[nums.size()];  // Result array
-        
-         for (int i = 0; i < nums.size(); i++) {
-            int num = nums.get(i);
-            int smallestPrime = -1;   
-             if ((num | 1) == 1) {
-                smallestPrime = 1;
-            } else {
-               
-                for (int p = 2; p < 1000; p++) {
-                    if (isPrime(p) && (num | p) == p) {
-                        smallestPrime = p;  
-                           break; 
-                    }
-                }
-            }
-            
-            ans[i] = smallestPrime; 
-        }
-        
-        return ans;
+        return res;
     }
 }
